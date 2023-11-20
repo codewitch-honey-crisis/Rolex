088a0f3c9f8d5a6c95d2dc28ebfbceafd99d6029
codewitch-honey-crisis <gaztoast@gmail.com>
2023-11-20 05:43:31 -0800

    updated demo

--- RolexDemoVB/Module1.vb
+++ RolexDemoVB/Module1.vb
@@ -3,17 +3,17 @@ Imports RolexDemoVB
 Module Module1
 
 	Sub Main()
-		Dim input As String = """base foo \""bar\"" foobar  bar 123 baz -345 fubar 1foo *#( 0"""
+		'Dim input As String = """base foo \""bar\"" foobar  bar 123 baz -345 fubar 1foo *#( 0"""
 
 		Using sr As StreamReader = File.OpenText("..\..\..\RolexDemo\Program.cs")
-			input = sr.ReadToEnd
+			Dim col As New RolexDemoVB.TextReaderEnumerable(sr)
+			Dim extokenizer As New RolexDemoVB.ExampleTokenizer(col)
+			For Each tok In extokenizer
+				If (-1 <> tok.SymbolId) Then
+					Console.WriteLine("{0}: {1} at column {2}", tok.SymbolId, tok.Value, tok.Column)
+				End If
+			Next
 		End Using
-		Dim extokenizer As New RolexDemoVB.ExampleTokenizer(input)
-		For Each tok In extokenizer
-			If (-1 <> tok.SymbolId) Then
-				Console.WriteLine("{0}: {1} at column {2}", tok.SymbolId, tok.Value, tok.Column)
-			End If
-		Next
 		Console.WriteLine()
 		Return
 
